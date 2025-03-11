eeda44144d4b8fdbcc8131cf459631f59dbd3fe1
bitsexploited <tspamiitesh@gmail.com>
2025-03-11 22:46:39 +0530

    Adding barbar plugin

--- nvim-config/lua/plugins/tabline.lua
+++ nvim-config/lua/plugins/tabline.lua
@@ -4,7 +4,19 @@ return {
     'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
   },
   config = function()
-    -- Keymaps for switching tabs
+    -- Corrected highlight group names
+    vim.api.nvim_set_hl(0, 'BufferLineSeparator', { fg = '#434c5e', bg = '#434c5e' })
+    vim.api.nvim_set_hl(0, 'BufferLineSeparatorVisible', { fg = '#434c5e', bg = 'NONE' })
+    vim.api.nvim_set_hl(0, 'BufferLineSeparatorInactive', { fg = '#434c5e', bg = 'NONE' })
+
+    require'barbar'.setup {
+      icons = {
+        separator = { left = '', right = '' }, -- Rounded separators
+        separator_at_end = true, -- Ensure separators are visible at the end
+      },
+    }
+
+    -- Keybindings for navigating buffers
     vim.api.nvim_set_keymap('n', '<Tab>', '<Cmd>BufferNext<CR>', { noremap = true, silent = true })
     vim.api.nvim_set_keymap('n', '<S-Tab>', '<Cmd>BufferPrevious<CR>', { noremap = true, silent = true })
   end,
