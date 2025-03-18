6965f79b32ac69a47c6f6478f7345df542395c47
bitsexploited <tspamiitesh@gmail.com>
2025-03-19 05:12:07 +0530

    nvim cmp path

--- nvim-config/lua/plugins/completions.lua
+++ nvim-config/lua/plugins/completions.lua
@@ -6,26 +6,22 @@ return {
     'L3MON4D3/LuaSnip',
     dependencies = {
       "saadparwaiz1/cmp_luasnip",
-      "rafamadriz/friendly-snippets"
+      "rafamadriz/friendly-snippets",
+      "hrsh7th/cmp-path"
     },
   },
   {
     "hrsh7th/nvim-cmp",
     config = function()
-      -- Enable true color support
-      vim.opt.termguicolors = true
 
-      -- Define custom highlight groups for completion window
       vim.api.nvim_set_hl(0, "CmpBorder", { fg = "#757165" }) -- Border color
       vim.api.nvim_set_hl(0, "Pmenu", { bg = "#2D353B", fg = "#cdd6f4" }) -- Background and foreground
       vim.api.nvim_set_hl(0, "PmenuSel", { bg = "#45475a", fg = "#f5e0dc" }) -- Selected item
 
-      -- Set up nvim-cmp
       local cmp = require'cmp'
       require("luasnip.loaders.from_vscode").lazy_load()
       cmp.setup({
         snippet = {
-          -- REQUIRED - you must specify a snippet engine
           expand = function(args)
             require('luasnip').lsp_expand(args.body)
           end,
@@ -45,13 +41,16 @@ return {
           ['<C-f>'] = cmp.mapping.scroll_docs(4),
           ['<C-Space>'] = cmp.mapping.complete(),
           ['<C-e>'] = cmp.mapping.abort(),
-          ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+          ['<CR>'] = cmp.mapping.confirm({ select = true }),
+          ['<TAB>'] = cmp.mapping(cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }), {'i'}),
+          ['<Up>'] = cmp.mapping(cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }), {'i'}),
         }),
         sources = cmp.config.sources({
           { name = 'nvim_lsp' },
-          { name = 'luasnip' }, -- For luasnip users.
+          { name = 'luasnip' }, 
         }, {
           { name = 'buffer' },
+          { name = 'path' }
         })
       })
     end,
