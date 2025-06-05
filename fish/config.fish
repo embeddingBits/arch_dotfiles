d5f62201faaf41e2303dcb9736b1b37b78ec7abb
BitsExploited <tspamiitesh@gmail.com>
2025-06-05 12:31:42 +0530

    Adding all the required files

--- fish/config.fish
+++ fish/config.fish
@@ -4,8 +4,16 @@ end
 set fish_greeting
 starship init fish | source
 fastfetch -c ~/.config/fastfetch/config.jsonc -l arch --logo-color-1 blue --logo-color-2 red
-
+set -x BAT_THEME Nord
 fish_add_path /home/amiitesh/.spicetify
+
+function all
+      sh swww-sh
+end
+
+function mon
+      sh ~/automation_scripts/external-wlr/output.sh
+end
 
 # bun
 set --export BUN_INSTALL "$HOME/.bun"
