5147a86b80d0533fb28b41d20772deed45ea70c7
BitsExploited <tspamiitesh@gmail.com>
2025-06-16 17:53:22 +0530

    Small Changes

--- nvim-config/init.lua
+++ nvim-config/init.lua
@@ -32,6 +32,11 @@ vim.g.maplocalleader = "\\"
 ------------------ Keybinds ------------------
 
 vim.keymap.set('n', '<leader>n', "<Cmd>enew<CR>", {})
+vim.keymap.set('n', '<leader>x', "<Cmd>bd<CR>", {})
+vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move to left window" })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move to below window" })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move to above window" })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to right window" })
 
 if vim.g.neovide then
     vim.opt.guifont = "JetBrainsMono Nerd Font:h14:b"
