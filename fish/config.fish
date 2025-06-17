79c349b91100261c80a96ecc8221100af2ab0b71
BitsExploited <tspamiitesh@gmail.com>
2025-06-17 20:13:23 +0530

    Small Changes

--- fish/config.fish
+++ fish/config.fish
@@ -15,6 +15,31 @@ function mon
       sh ~/automation_scripts/external-wlr/output.sh
 end
 
-# bun
+function ls
+    set -l exa_args
+
+    for arg in $argv
+        switch $arg
+            case '-l'
+                set exa_args $exa_args '--long'
+            case '-a'
+                set exa_args $exa_args '--all'
+            case '-la' '-al'
+                set exa_args $exa_args '--long' '--all'
+            case '-h'
+                set exa_args $exa_args '--header'
+            case '-t'
+                set exa_args $exa_args '--sort=modified'
+            case '-r'
+                set exa_args $exa_args '--reverse'
+            case '*'
+                set exa_args $exa_args $arg
+        end
+    end
+
+    exa --icons $exa_args
+end
+
+
 set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
