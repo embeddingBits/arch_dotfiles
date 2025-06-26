f913fa6dfcefdc72b8965009cb29fbcfaef38ce0
embeddingbits <tspamiitesh@gmail.com>
2025-06-26 13:18:12 +0000

    small changes

--- pkgs.sh
+++ pkgs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo pacman -S --needed --noconfirm hyprland hyprlock hyprshot hyprpicker swww nwg-look rofi-wayland dunst cmus neovim kitty fastfetch fish starship cava btop ranger ncmpcpp rmpc gcc git zig go nodejs python python-pip firefox discord pavucontrol chromium github-cli fd ripgrep make zip unzip tree clang libnotify brightnessctl power-profiles-daemon pandoc mpd mpc blueman bluez-utils eza kvantum ttf-jetbrainsmono-nerd qt5ct qt6ct
+sudo pacman -S --needed --noconfirm hyprland hyprlock hyprshot hyprpicker swww nwg-look rofi-wayland dunst cmus neovim kitty fastfetch fish starship cava btop ranger ncmpcpp rmpc gcc git zig go nodejs python python-pip firefox discord pavucontrol chromium github-cli fd ripgrep make zip unzip tree clang libnotify brightnessctl power-profiles-daemon pandoc mpd mpc blueman bluez-utils eza kvantum ttf-jetbrainsmono-nerd qt5ct qt6ct wlr-randr
 
 yay -S --needed --noconfirm waypaper spotify rofi-power-menu waybar-cava
