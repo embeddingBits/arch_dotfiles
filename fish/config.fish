38b43b19c5b1853f523a0b8054a4c47fee3cbe91
EmbeddingBits <tspamiitesh@gmail.com>
2025-06-26 09:35:41 +0530

    Adding dister config

--- fish/config.fish
+++ fish/config.fish
@@ -3,17 +3,9 @@ if status is-interactive
 end
 set fish_greeting
 starship init fish | source
-fastfetch -c ~/.config/fastfetch/config.jsonc -l arch --logo-color-1 blue --logo-color-2 red
+fastfetch -c ~/.config/fastfetch/config.jsonc --logo-color-1 blue --logo-color-2 red
 set -x BAT_THEME Nord
 fish_add_path /home/amiitesh/.spicetify
-
-function all
-      sh swww-sh
-end
-
-function mon
-      sh ~/automation_scripts/external-wlr/output.sh
-end
 
 function ls
     set -l exa_args
