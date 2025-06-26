1a0153fdb0ab6b37c8be1a68a0e439322b7f007a
embeddingbits <tspamiitesh@gmail.com>
2025-06-26 11:17:01 +0000

    small changes

--- nvim-config/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim-config/.config/nvim/lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "zls", "gopls", "verible" }
+        ensure_installed = { "lua_ls", "clangd", "zls", "gopls", "verible" }
       })
       end
   },
@@ -18,8 +18,10 @@ return {
     config = function()
       local capabilities = require('cmp_nvim_lsp').default_capabilities()
       local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities
+    })
       lspconfig.clangd.setup({ capabilities = capabilities })
-      lspconfig.lua_ls.setup({ capabilities = capabilities })
       lspconfig.zls.setup({ capabilities = capabilities })
       lspconfig.gopls.setup({ capabilities = capabilities })
       lspconfig.verible.setup({ capabilities = capabilities })
--- nvim-config/.config/nvim/lua/plugins/tabline.lua
+++ nvim-config/.config/nvim/lua/plugins/tabline.lua
@@ -3,5 +3,18 @@ return {
       dependencies = {
             'nvim-tree/nvim-web-devicons',
       },
+      config = function()
+            local bufferline = require("bufferline")
+
+            bufferline.setup {
+                  options = {
+                        mode = "buffers",
+                        separator_style = "slant",  -- Simple separators to check
+                        color_icons = true
+
+                  },
+                  highlights = require("nord.plugins.bufferline").akinsho(),
+            }
+      end
 }
 
