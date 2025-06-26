3952180813d24262c27debaa12cfc9df8a2ab4f5
embeddingbits <tspamiitesh@gmail.com>
2025-06-26 11:08:51 +0000

    Small Changes

--- nvim-config/.config/nvim/lua/plugins/tabline.lua
+++ nvim-config/.config/nvim/lua/plugins/tabline.lua
@@ -3,17 +3,5 @@ return {
       dependencies = {
             'nvim-tree/nvim-web-devicons',
       },
-      config = function()
-            local bufferline = require("bufferline")
-
-            bufferline.setup {
-                  options = {
-                        mode = "buffers",
-                        separator_style = "slant",  -- Simple separators to check
-                        color_icons = true
-
-                  },
-            }
-      end
 }
 
