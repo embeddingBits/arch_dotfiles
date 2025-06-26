33adb9d3fa53032b9d3b703edcc6825844e19c99
embeddingbits <tspamiitesh@gmail.com>
2025-06-26 07:02:25 +0000

    Small Changes

--- nvim-config/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim-config/.config/nvim/lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "zls", "gopls", "verible", "ast_grep" }
+        ensure_installed = { "zls", "gopls", "verible" }
       })
       end
   },
@@ -18,7 +18,6 @@ return {
     config = function()
       local capabilities = require('cmp_nvim_lsp').default_capabilities()
       local lspconfig = require("lspconfig")
-      lspconfig.ast_grep.setup({ capabilities = capabilities })
       lspconfig.clangd.setup({ capabilities = capabilities })
       lspconfig.lua_ls.setup({ capabilities = capabilities })
       lspconfig.zls.setup({ capabilities = capabilities })
