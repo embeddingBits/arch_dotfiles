3791c06c1e286238e6088b5190969e68dc8b42ea
embeddingbits <tspamiitesh@gmail.com>
2025-08-17 19:42:04 +0530

    All gruvbox rice

--- nvim-config/.config/nvim/lua/plugins/nord.lua
+++ nvim-config/.config/nvim/lua/plugins/nord.lua
@@ -1,8 +1,15 @@
 return {
-      "gbprod/nord.nvim", 
-      lazy = false,
-      priority = 1000,
-      config = function()
-            vim.cmd("colorscheme nord")
-      end,
+      {
+            "gbprod/nord.nvim", 
+            lazy = false,
+            priority = 1000,
+            config = function()
+            end,
+      },
+
+      { 
+            "ellisonleao/gruvbox.nvim", 
+            priority = 1000,
+            config = true,
+      }
 }
