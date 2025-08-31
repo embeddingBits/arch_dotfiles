5ba4e7489bcecc96718e72c7910bbd5abc7d831b
embeddingbits <tspamiitesh@gmail.com>
2025-08-31 15:58:13 +0530

    small changes

--- stow_apply.sh
+++ stow_apply.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-stow btop/ cava/ dunst_gruv/ fastfetch/ hypr/ kitty/ mpd/ nvim-config/ quickshell/ qutebrowser/ ranger/ rmpc/ rofi/ starship_gruvbox/ tmux/ waybar/ zathura_gruv/
+stow btop/ cava/ dunst_gruv/ fastfetch/ hypr/ fish/ kitty/ mpd/ nvim-config/ quickshell/ qutebrowser/ ranger/ rmpc/ rofi/ starship_gruvbox/ tmux/ waybar/ zathura_gruv/
