d01856da18a9ce75a3b1cb2ce49876405469bba5
embeddingbits <tspamiitesh@gmail.com>
2025-09-02 21:27:00 +0530

    Adding River Config

---
+++ river/.config/river/env.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+export XDG_CURRENT_DESKTOP=River
+export XDG_SESSION_DESKTOP=River
+export XDG_SESSION_TYPE=wayland
+export MOZ_ENABLE_WAYLAND=1
+export QT_QPA_PLATFORM="wayland;xcb"
+export QT_QPA_PLATFORMTHEME=qt6ct
+export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
+export QT_SCALE_FACTOR=1
+export QT_AUTO_SCREEN_SCALE_FACTOR=1
+export GDK_SCALE=1
+export GDK_BACKEND="wayland,x11,*"
+export WLR_NO_HARDWARE_CURSORS=1
