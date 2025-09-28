c63524390bd0c52683e1109bacd3a3d046f07c52
embeddingbits <tspamiitesh@gmail.com>
2025-09-28 10:33:10 +0530

    Restructuring the dots

---
+++ gruvbox_config/.config/qutebrowser/config.py
@@ -0,0 +1,15 @@
+config.load_autoconfig()
+
+c.fonts.default_family = 'JetBrainsMono Nerd Font'
+c.fonts.default_size = '12pt'
+c.fonts.web.family.standard = "JetBrainsMono Nerd Font"
+c.fonts.web.family.fixed = "JetBrainsMono Nerd Font"
+c.fonts.web.family.fantasy = "JetBrainsMono Nerd Font"
+c.fonts.web.family.serif = "JetBrainsMono Nerd Font"
+c.fonts.web.family.sans_serif = "JetBrainsMono Nerd Font"
+c.fonts.web.size.default = 16
+c.fonts.web.size.default_fixed = 14
+c.fonts.web.size.minimum = 8
+#config.set("colors.webpage.darkmode.enabled", True)
+
+config.source('gruvbox.py')
