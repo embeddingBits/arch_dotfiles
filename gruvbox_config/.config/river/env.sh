056731b192e9b70ae641d0fb1c8ae9c86bda5610
ebits <tspamiitesh@gmail.com>
2025-10-23 22:29:34 +0530

    Adding new env vars for RiverWM

--- gruvbox_config/.config/river/env.sh
+++ gruvbox_config/.config/river/env.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export XDG_CURRENT_DESKTOP=River
+export XDG_CURRENT_DESKTOP=river
 export XDG_SESSION_DESKTOP=River
 export XDG_SESSION_TYPE=wayland
 export MOZ_ENABLE_WAYLAND=1
@@ -13,3 +13,8 @@ export GDK_SCALE=1
 export GDK_BACKEND="wayland,x11,*"
 export WLR_NO_HARDWARE_CURSORS=1
 export GTK_CSD=0
+
+dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP XDG_SESSION_TYPE XDG_SESSION_DESKTOP
+
+systemctl --user start xdg-desktop-portal-wlr
+systemctl --user start xdg-desktop-portal
