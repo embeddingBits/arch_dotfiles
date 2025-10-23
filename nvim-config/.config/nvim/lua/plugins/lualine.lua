2cf006e3c0bde5324639c8709f4a4e713f3e6459
ebits <tspamiitesh@gmail.com>
2025-10-23 15:12:22 +0530

    Adding extra neovim configs

--- nvim-config/.config/nvim/lua/plugins/lualine.lua
+++ nvim-config/.config/nvim/lua/plugins/lualine.lua
@@ -3,18 +3,21 @@ return {
       dependencies = { 'nvim-tree/nvim-web-devicons' },
       config = function()
 
-            local nord = require('lualine.themes.nord')
+            local gruvbox = require('lualine.themes.gruvbox')
 
-            nord.normal.a.bg = '#81a1c1' 
-            nord.normal.a.fg = '#2E3440'  
-            nord.normal.a.gui = 'bold'
-            nord.insert.a.bg = '#ba6a72'  
-            nord.insert.a.fg = '#2E3440'  
-            nord.insert.a.gui = 'bold'
-            nord.command = {
+            gruvbox.normal.a.bg = '#83a598'
+            gruvbox.normal.a.fg = '#1d2021'
+            gruvbox.normal.a.gui = 'bold'
+            gruvbox.insert.a.bg = '#a9b665'
+            gruvbox.insert.a.fg = '#1d2021'
+            gruvbox.insert.a.gui = 'bold'
+            gruvbox.visual.a.bg = '#d3869b'
+            gruvbox.visual.a.fg = '#1d2021'
+            gruvbox.visual.a.gui = 'bold'
+            gruvbox.command = {
                   a = {
-                        fg = '#2E3440',
-                        bg = '#D08770',
+                        fg = '#1d2021',
+                        bg = '#ea6962',
                         gui = 'bold',
                   }
             }
@@ -23,7 +26,7 @@ return {
             require('lualine').setup {
                   options = {
                         icons_enabled = true,
-                        theme = auto,
+                        theme = gruvbox,
                         component_separators = { left = '|', right = '|'},
                         section_separators = { left = '', right = ''},
                         disabled_filetypes = {
@@ -57,12 +60,27 @@ return {
                         lualine_a = {
                               {
                                     'mode',
-                                    icon = ' ',  -- before the mode text (use any Nerd Font icon)
                               }
                         },
-                        lualine_b = {'branch', 'diff'},
-                        lualine_c = {'filename'},
-                        lualine_x = {'filetype'},
+                        lualine_b = {'branch', 'diff', 'diagnostics',
+                              {
+					"buffers",
+					buffers_color = {
+						active = { bg = '#D8A657' , fg = '#1d2021', gui = "bold" },
+						inactive = { bg = '#a89984', fg = '#282828', gui = "italic" },
+					},
+					symbols = {
+						modified = " ●",
+						alternate_file = "",
+						directory = "",
+					},
+					mode = 0,
+				},
+                        },
+                        lualine_c = {
+                              'filename',
+                        },
+                        lualine_x = {'encoding', 'filetype'},
                         lualine_y = {'progress'},
                         lualine_z = {'location'}
                   },
