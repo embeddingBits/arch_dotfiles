483805c7a39861ee34b76aa2272d5c5780c710ad
ebits <tspamiitesh@gmail.com>
2025-12-31 13:34:15 +0530

    Major changes

--- gruvbox_config/.config/waybar/scripts/script.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-speedtest-cli --simple | awk '/Download/ { d=$2 } /Upload/ { u=$2 } END { printf "↓ %.1f Mbps ↑ %.1f Mbps\n", d, u }'
--- nvim-config/.config/nvim/README.md
+++
@@ -1,4 +0,0 @@
-# My usual Neovim Configuration 
-
-![dashboard1](./dashboard1.png)
-![syntax1](./syntax1.png)
