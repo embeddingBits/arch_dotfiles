cb775be55de426a5b1ed537346eed584559df0cd
ebits <tspamiitesh@gmail.com>
2026-01-01 14:47:22 +0530

    Organising fish config

---
+++ gruvbox_config/.config/fish/aliases.fish
@@ -0,0 +1,12 @@
+function fetch --wraps='fastfetch -c ~/.config/fastfetch/small.jsonc --logo-color-1 blue --logo-color-2 red' --description 'alias fetch fastfetch -c ~/.config/fastfetch/small.jsonc --logo-color-1 blue --logo-color-2 red'
+    fastfetch -c ~/.config/fastfetch/small.jsonc --logo-color-1 blue --logo-color-2 red $argv
+end
+
+function ls --wraps=exa --wraps='exa --icons' --description 'alias ls exa --icons'
+    exa --icons $argv
+end
+
+function nv --wraps=nvim --description 'alias nv nvim'
+    nvim $argv
+end
+
--- gruvbox_config/.config/fish/config.fish
+++ gruvbox_config/.config/fish/config.fish
@@ -4,52 +4,10 @@ set -x BAT_THEME gruvbox-dark
 set -x EDITOR nvim
 set -x TERMINAL ghostty
 
-function fetch
-      fastfetch -c ~/.config/fastfetch/small.jsonc --logo-color-1 blue --logo-color-2 red
-end
+source ~/.config/fish/aliases.fish
 
-function mirrorOut
-      wlr-randr --output HDMI-A-1 --mode 1920x1080 --pos 0,0
-end
-
-function dual
-      wlr-randr --output HDMI-A-1 --pos 0,0 --output eDP-1 --pos 1920,0
-end
-
-function nv
-      nvim $argv
-end
-
-function ls
-    set -l exa_args
-
-    for arg in $argv
-        switch $arg
-            case '-l'
-                set exa_args $exa_args '--long'
-            case '-a'
-                set exa_args $exa_args '--all'
-            case '-la' '-al'
-                set exa_args $exa_args '--long' '--all'
-            case '-h'
-                set exa_args $exa_args '--header'
-            case '-t'
-                set exa_args $exa_args '--sort=modified'
-            case '-r'
-                set exa_args $exa_args '--reverse'
-            case '*'
-                set exa_args $exa_args $arg
-        end
-    end
-
-    exa --icons $exa_args
-end
-
+# ssh agent
 set -gx SSH_AUTH_SOCK $XDG_RUNTIME_DIR/ssh-agent.socket
-
-if status is-interactive
-    ssh-add -q ~/.ssh/id_ed25519 2>/dev/null
-end
 
 set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
