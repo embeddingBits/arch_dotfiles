8cdb900b3c9d1887941453012c65bee5ddbdacf5
ebits <tspamiitesh@gmail.com>
2026-01-08 19:20:11 +0530

    Major: Fastfetch, fish aliases and dunst

--- gruvbox_config/.config/fish/config.fish
+++ gruvbox_config/.config/fish/config.fish
@@ -13,3 +13,7 @@ set -gx SSH_AUTH_SOCK $XDG_RUNTIME_DIR/ssh-agent.socket
 set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
 source /home/ebits/.config/fish/completions/inLimbo.fish
+set -Ux GOPATH $HOME/.local/go
+set -Ux GOBIN  $HOME/.local/go/bin
+fish_add_path  $GOBIN
+
