db9e9a93411d3577db3e6170e3c4162d5463ac3d
ebits <tspamiitesh@gmail.com>
2026-05-31 09:01:08 +0530

    Update

---
+++ gruvbox_config/.config/niri/scripts/swaylock.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+swaylock \
+       --screenshots \
+       --clock \
+       --indicator \
+       --indicator-radius 100 \
+       --indicator-thickness 7 \
+       --effect-blur 7x5 \
+       --effect-vignette 0.5:0.5 \
+       --ring-color 7daea3 \
+       --key-hl-color dfa65800 \
+       --line-color 1d2021 \
+       --inside-color 00000088 \
+       --separator-color 00000000 \
+       --grace 2 \
+       --fade-in 0
