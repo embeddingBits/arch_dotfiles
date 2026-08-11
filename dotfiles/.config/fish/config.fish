7a688e002374ce08b31902bf28aa4f2a8dc6ac5a
ebits <tspamiitesh@gmail.com>
2026-08-11 16:36:06 +0530

    update

--- dotfiles/.config/fish/config.fish
+++ dotfiles/.config/fish/config.fish
@@ -7,11 +7,32 @@ set -x TERMINAL footclient
 source ~/.config/fish/aliases.fish
 zoxide init fish | source
 
-# Start SSH agent if not already running
-if not set -q SSH_AUTH_SOCK
-    eval (ssh-agent -c)
-    set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
-    set -Ux SSH_AGENT_PID $SSH_AGENT_PID
+# SSH agent: one agent per login, key unlocked lazily on first git/ssh use
+if test -f "$HOME/.ssh/agent-env"
+    set -gx SSH_AUTH_SOCK (sed -n 's/^SSH_AUTH_SOCK=\([^;]*\);.*/\1/p' "$HOME/.ssh/agent-env")
+    set -gx SSH_AGENT_PID (sed -n 's/^SSH_AGENT_PID=\([^;]*\);.*/\1/p' "$HOME/.ssh/agent-env")
+end
+if not test -S "$SSH_AUTH_SOCK"
+    ssh-agent > "$HOME/.ssh/agent-env" 2>/dev/null
+    and begin
+        set -gx SSH_AUTH_SOCK (sed -n 's/^SSH_AUTH_SOCK=\([^;]*\);.*/\1/p' "$HOME/.ssh/agent-env")
+        set -gx SSH_AGENT_PID (sed -n 's/^SSH_AGENT_PID=\([^;]*\);.*/\1/p' "$HOME/.ssh/agent-env")
+    end
+end
+
+function __ensure_ssh_key
+    set -q SSH_AUTH_SOCK; and test -S "$SSH_AUTH_SOCK"; or return 1
+    ssh-add -l >/dev/null 2>&1; or ssh-add "$HOME/.ssh/git" 2>/dev/null
+end
+
+function git --wraps=git
+    __ensure_ssh_key
+    command git $argv
+end
+
+function ssh --wraps=ssh
+    __ensure_ssh_key
+    command ssh $argv
 end
 
 # Zig Version Manager
